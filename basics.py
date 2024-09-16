4ac6e01db65e7abd5513863b15997f80a273232f
Rohit <148680813+Rohitsail@users.noreply.github.com>
2024-09-16 10:07:44 +0530

    Delete basics.py

--- basics.py
+++
@@ -1,7 +0,0 @@
-for r in range(7):
-    for c in range(5):
-        if (c==0) or (c==4 and (r!=0 and r!=3 and r!=6)) or ((r==0 or r==3 or r==6) and (0<c<4)):
-            print("*",end="")
-        else:
-            print(end=" ")
-    print()
